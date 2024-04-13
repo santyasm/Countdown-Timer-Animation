fd451cccd9c865cef3347d5b4b47f994dd821fb7
santyasm <yyasant5@gmail.com>
2024-04-13 19:47:45 -0300

    first commit

---
+++ src/Timer/Timer.tsx
@@ -0,0 +1,117 @@
+import * as React from 'react';
+import {
+  Vibration,
+  StatusBar,
+  Easing,
+  TextInput,
+  Dimensions,
+  Animated,
+  TouchableOpacity,
+  FlatList,
+  Text,
+  View,
+  StyleSheet,
+} from 'react-native';
+const { width, height } = Dimensions.get('window');
+const colors = {
+  black: '#323F4E',
+  red: '#F76A6A',
+  text: '#ffffff',
+};
+
+const timers = [...Array(13).keys()].map(i => (i === 0 ? 1 : i * 5));
+const ITEM_SIZE = width * 0.38;
+const ITEM_SPACING = (width - ITEM_SIZE) / 2;
+
+export const Timer: React.FC = () => {
+  const scrollX = React.useRef(new Animated.Value(0)).current;
+
+  return (
+    <View style={styles.container}>
+      <StatusBar hidden />
+      <Animated.View
+        style={[
+          StyleSheet.absoluteFillObject,
+          {
+            justifyContent: 'flex-end',
+            alignItems: 'center',
+            paddingBottom: 100,
+          },
+        ]}>
+        <TouchableOpacity onPress={() => {}}>
+          <View style={styles.roundButton} />
+        </TouchableOpacity>
+      </Animated.View>
+      <View
+        style={{
+          position: 'absolute',
+          top: height / 3,
+          left: 0,
+          right: 0,
+          flex: 1,
+        }}>
+        <Animated.FlatList
+          data={timers}
+          keyExtractor={item => item.toString()}
+          style={{ flexGrow: 0 }}
+          decelerationRate="fast"
+          snapToInterval={ITEM_SIZE}
+          contentContainerStyle={{
+            paddingHorizontal: ITEM_SPACING,
+          }}
+          renderItem={({ item, index }) => {
+            const inputRange = [
+              (index - 1) * ITEM_SIZE,
+              index * ITEM_SIZE,
+              (index + 1) * ITEM_SIZE,
+            ];
+
+            const opacity = scrollX.interpolate({
+              inputRange,
+              outputRange: [0.4, 1, 0.4],
+            });
+
+            return (
+              <View
+                style={{
+                  width: ITEM_SIZE,
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                }}>
+                <Animated.Text style={[styles.text, { opacity }]}>
+                  {item}
+                </Animated.Text>
+              </View>
+            );
+          }}
+          horizontal
+          bounces={false}
+          showsHorizontalScrollIndicator={false}
+          onScroll={Animated.event(
+            [{ nativeEvent: { contentOffset: { x: scrollX } } }],
+            { useNativeDriver: true },
+          )}
+        />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.black,
+  },
+  roundButton: {
+    width: 80,
+    height: 80,
+    borderRadius: 80,
+    backgroundColor: colors.red,
+  },
+  text: {
+    fontSize: ITEM_SIZE * 0.8,
+    fontFamily: 'Menlo',
+    color: colors.text,
+    fontWeight: '900',
+  },
+});
